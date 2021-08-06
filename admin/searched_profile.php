90cfdc41725f219c42fd2b64e0545926a80e8fe5
igniteking <khanzaidan786@gmail.com>
2021-08-06 16:58:09 +0530

    done

--- admin/searched_profile.php
+++ admin/searched_profile.php
@@ -42,7 +42,59 @@ while ($rows = mysqli_fetch_assoc($result2)) {
     $status = $rows['status'];
 ?>
 <html>
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
+<html>
+
 <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Vishwavedika - Application Form</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="">
+</head>
+<style type="text/css">
+    @import url("https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;500;600;400;700;800&display=swap");
+    @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;500;600;700;900&display=swap");
+
+    * {
+        padding: 0px;
+        margin: 0px;
+    }
+
+    body {
+        background: #e3eaee;
+    }
+
+    .header_main {
+        width: 100%;
+        height: 64px;
+        z-index: 10000;
+        background: #fff;
+        border-bottom: 1px solid #eee;
+        -webkit-box-shadow: 10px 10px 7px -9px rgba(199, 199, 199, 1);
+        -moz-box-shadow: 10px 10px 7px -9px rgba(199, 199, 199, 1);
+        box-shadow: 10px 10px 7px -9px rgba(199, 199, 199, 1);
+    }
+
+    #wrapper {
+        margin-right: auto;
+        margin-left: auto;
+        max-width: 1200px;
+    }
+</style>
+
+<body>
+    <div class="header_main">
+        <div id='wrapper'>
+            <a href='https://www.vishwavedika.net/'><img src='images/logo.png' width="50px" style="margin-left: 10px; float: left; margin-top: 5px;"></a>
+            <a href='' style="margin-right: 10px; float: right; color: #fff; text-decoration: none; font-family: Roboto; background: #d0229d; padding: 10px; border-radius: 4px; margin-top: 12px;">Contact Us</a>
+        </div>
+    </div>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Dashboard</title>
