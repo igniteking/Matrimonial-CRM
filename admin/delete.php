27dd44484b2b91a6f9d713f09c1a847e5d6c4e8d
Rohan Sidhu <44905914+sidhuroh@users.noreply.github.com>
2021-08-26 16:38:15 +0530

    Touch

--- admin/delete.php
+++ admin/delete.php
@@ -1,14 +1,11 @@
-<?php include_once("connection/db.php"); ?>
-<?php
-
-$user_id = $_GET['id']; // get id through query string
+<?php include_once("connection/db.php");
+$user_id = @$_GET['id']; // get id through query string
 $query = "DELETE FROM `users` WHERE `id` = '$user_id'";
 $del = mysqli_query($conn, $query); // delete query
 if ($del) {
     mysqli_close($conn); // Close connection
     echo "<meta http-equiv=\"refresh\" content=\"0; url=index.php\">"; // redirects to all records page
-    exit;
+    exit();
 } else {
     echo "Error deleting record"; // display error message if not delete
 }
-?>
---
+++ nxhbalz7.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Vishwavedika - Application Form</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <!-- JavaScript Bundle with Popper -->
+    <link href="dist/cropper.min.css" rel="stylesheet" type="text/css" />
+    <link href="dist/bootstrap.min.css" rel="stylesheet" type="text/css" />
+</head>
+<style type='text/css'>
+
+</style>
+
+<body>
+    <div class="header_main">
+        <div id='wrapper'>
+            <a href='https://www.vishwavedika.net/'><img src='img/logo.png' width="150px" style="margin-left: 10px; float: left; margin-top: 5px;"></a>
+            <a href='' style="margin-right: 10px; float: right; color: #fff; text-decoration: none; font-family: Roboto; background: #d0229d; padding: 10px; border-radius: 4px; margin-top: 12px;">Contact Us</a>
+        </div>
+    </div>
+    <br><br>
+    <div id='wrapper'>
+        <div style='width: 100%; background: #fff; border: 1px solid #eee; background: #fff; border-radius: 8px;'>
+            <div style='padding: 10px;'>
+                <div class='flex-container'>
+                    <div style='flex-grow: 1;'>Input 1</div>
+                    <div style='flex-grow: 1;'>Input 1</div>
+                    <div style='flex-grow: 1;'>Input 1</div>
+                    <div style='flex-grow: 1;'>Input 1</div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
